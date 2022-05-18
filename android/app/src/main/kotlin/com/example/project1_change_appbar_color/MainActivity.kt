6ab4ebc5cd6101cf5e335ef54a16b73058f041e8
MertSaygili <mert71719601@gmail.com>
2022-05-18 23:16:17 +0300

    first init

---
+++ android/app/src/main/kotlin/com/example/project1_change_appbar_color/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project1_change_appbar_color
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'Classes/Projects/change_appbar_color.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ChangeAppBarColor(),
+    );
+  }
+}
