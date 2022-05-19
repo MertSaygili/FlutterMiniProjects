a2113df459c7bbd538fbef9398ab9f4fd79144f2
MertSaygili <mert71719601@gmail.com>
2022-05-19 23:25:05 +0300

    check

--- lib/Classes/Projects/card_widget/using_card_widget.dart
+++ lib/Classes/Projects/card_widget/using_card_widget.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class UsinCardWidget extends StatelessWidget {
-  const UsinCardWidget({Key? key}) : super(key: key);
+class DesingningCardWidget extends StatelessWidget {
+  const DesingningCardWidget({Key? key}) : super(key: key);
+  final String _appBarTitle = "Designing Card Widget";
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(_appBarTitle)),
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+      ),
+      body: Padding(
+        padding: PaddingItems().paddingPageNormal,
+        child: Column(
+          children: const [],
+        ),
+      ),
+    );
   }
+}
+
+class PaddingItems {
+  final EdgeInsets paddingPageNormal = EdgeInsets.symmetric(
+      vertical: PaddingAttributes()._verticalSmall,
+      horizontal: PaddingAttributes()._horizontalSmall);
+}
+
+class PaddingAttributes {
+  final double _verticalSmall = 10;
+  final double _horizontalSmall = 8;
 }
