4bd54fff359c87fe50df96bc44317cd3e3909cef
MertSaygili <mert71719601@gmail.com>
2022-05-23 21:12:10 +0300

    started to designing simple login page

---
+++ lib/Classes/Projects/trext_field_widget.dart/simple_login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SimpleLoginPage extends StatefulWidget {
+  const SimpleLoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<SimpleLoginPage> createState() => _SimpleLoginPageState();
+}
+
+class _SimpleLoginPageState extends State<SimpleLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: const [],
+      ),
+    );
+  }
+}
