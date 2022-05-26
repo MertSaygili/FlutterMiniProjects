75e233b36384fa946152b8a6b29d9373ffc84fc6
MertSaygili <mert71719601@gmail.com>
2022-05-26 17:39:29 +0300

    i have error at line 97 parsing error

--- lib/Classes/Projects/text_field_widget.dart/password_strenght_level.dart
+++ lib/Classes/Projects/text_field_widget.dart/password_strenght_level.dart
@@ -11,10 +11,11 @@ class PasswordStrengthLevel extends StatefulWidget {
 
 class _PasswordStrengthLevelState extends State<PasswordStrengthLevel> {
   final String _appBarTitle = "Password Strength Level";
+  final String _hintLabelText = "password";
+  final double width = 0;
+  final int _maxLength = 20;
   bool _isObscure = false;
   String? _password;
-
-  final int maxLength = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -25,32 +26,36 @@ class _PasswordStrengthLevelState extends State<PasswordStrengthLevel> {
           Padding(
             padding: PaddingSettings().paddingTextField,
             child: TextField(
-              maxLength: 20,
-              keyboardType: TextInputType.number,
+              maxLength: _maxLength,
+              keyboardType: TextInputType.visiblePassword,
               textAlign: TextAlign.start,
               textInputAction: TextInputAction.go,
               autofocus: true,
               obscureText: _isObscure,
+              buildCounter: (BuildContext context,
+                  {int? currentLength, bool? isFocused, int? maxLength}) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    AnimatedContainer(
+                      key: UniqueKey(),
+                      duration: const Duration(seconds: 2),
+                      height: 20,
+                      width: width + (currentLength! * 10.0),
+                      color: Colors.green,
+                    ),
+                  ],
+                );
+              },
               onChanged: (text) => setState(() {
-                print(text);
+                CheckPassword().checkPassword(text);
               }),
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
+                border: outlineInputBorder(),
                 prefixIcon: const Icon(Icons.key),
-                suffixIcon: IconButton(
-                  icon: _isObscure
-                      ? const Icon(Icons.visibility)
-                      : const Icon(Icons.visibility_off),
-                  onPressed: () {
-                    setState(() {
-                      _isObscure = !_isObscure;
-                    });
-                  },
-                ),
-                hintText: "password",
-                labelText: "password",
+                suffixIcon: visibilityOfButton(),
+                hintText: _hintLabelText,
+                labelText: _hintLabelText,
                 fillColor: Theme.of(context).colorScheme.onBackground,
               ),
             ),
@@ -59,19 +64,53 @@ class _PasswordStrengthLevelState extends State<PasswordStrengthLevel> {
       ),
     );
   }
+
+  double calculateWidth() {
+    return 1;
+  }
+
+  OutlineInputBorder outlineInputBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+    );
+  }
+
+  IconButton visibilityOfButton() {
+    return IconButton(
+      icon: _isObscure
+          ? const Icon(Icons.visibility)
+          : const Icon(Icons.visibility_off),
+      onPressed: () {
+        setState(() {
+          _isObscure = !_isObscure;
+        });
+      },
+    );
+  }
 }
 
 class CheckPassword {
-  void checkPassword(String password) {
-    if (password.contains('@')) {
-      // ignore: avoid_print
-      print("yaho");
+  int checkPassword(String password) {
+    int passwordPoint = 0;
+
+    for (int i = 0; i < password.length; i++) {
+      int assciCodeOfLetter = password[i] as int;
+
+      if ((assciCodeOfLetter >= 32 && assciCodeOfLetter <= 47) ||
+          (assciCodeOfLetter >= 58 && assciCodeOfLetter <= 64) ||
+          (assciCodeOfLetter >= 91 && assciCodeOfLetter <= 96) ||
+          (assciCodeOfLetter >= 123 && assciCodeOfLetter <= 126)) {
+        passwordPoint = passwordPoint + LevelOfPasswordChars()._signSize;
+      }
     }
+    print(passwordPoint);
+
+    return 0;
   }
 }
 
 class LevelOfPasswordChars {
-  final int _signSize = 100;
+  final int _signSize = 50;
   final int _charSizeLowerCase = 10;
   final int _charSizeUpperCase = 10;
   final int _intSize = 10;
