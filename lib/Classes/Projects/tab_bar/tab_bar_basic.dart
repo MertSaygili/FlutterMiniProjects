0c541ba674bae51704ad6e9ac15da3b43f70a1fb
MertSaygili <mert71719601@gmail.com>
2022-06-27 22:40:33 +0300

    basic tabBar begginig

---
+++ lib/Classes/Projects/tab_bar/tab_bar_basic.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -23,4 +23,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-// deneme
+// denemeasdasdsad
