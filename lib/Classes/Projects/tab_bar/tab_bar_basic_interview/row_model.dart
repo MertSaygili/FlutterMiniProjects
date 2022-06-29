f1f8e2dc6e91781a8e65cea73785b9166acc77f3
MertSaygili <mert71719601@gmail.com>
2022-06-29 12:27:51 +0300

    row model

---
+++ lib/Classes/Projects/tab_bar/tab_bar_basic_interview/row_model.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:project1_change_appbar_color/Classes/Projects/tab_bar/tab_bar_basic_interview/text.dart';
+
+class RowModel extends StatefulWidget {
+  const RowModel({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  State<RowModel> createState() => _RowModelState();
+}
+
+class _RowModelState extends State<RowModel> {
+  late final String _text;
+  bool _checkBox = false;
+  int check = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _text = widget.text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Transform.scale(
+          scale: 1.3,
+          child: Checkbox(
+            side: const BorderSide(color: Colors.blueAccent),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            value: _checkBox,
+            onChanged: (value) {
+              setState(() {
+                _checkBox = !_checkBox;
+              });
+            },
+          ),
+        ),
+        Text(_text),
+      ],
+    );
+  }
+}
