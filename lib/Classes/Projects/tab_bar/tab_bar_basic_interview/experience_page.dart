346de6d0236ec355c62b5e60a11a5b4c79b7a5f1
MertSaygili <mert71719601@gmail.com>
2022-07-27 20:02:21 +0300

    string folder

--- lib/Classes/Projects/tab_bar/tab_bar_basic_interview/experience_page.dart
+++ lib/Classes/Projects/tab_bar/tab_bar_basic_interview/experience_page.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
 class ExperiencePageView extends StatefulWidget {
-  const ExperiencePageView({Key? key}) : super(key: key);
+  const ExperiencePageView({Key? key, required this.controller})
+      : super(key: key);
+  final TabController controller;
 
   @override
   State<ExperiencePageView> createState() => _ExperiencePageViewState();
 }
 
 class _ExperiencePageViewState extends State<ExperiencePageView> {
+  bool _throwShotAway = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          CheckboxListTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            title: const Text('merhaba cnaim'),
+            value: _throwShotAway,
+            onChanged: (bool? newValue) {
+              setState(() {
+                _throwShotAway = !_throwShotAway;
+              });
+            },
+          )
+        ],
+      ),
+    );
   }
 }
