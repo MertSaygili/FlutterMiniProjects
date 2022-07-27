1d625289755d01709c4d567f05ea58f2111d648d
MertSaygili <mert71719601@gmail.com>
2022-07-27 13:12:27 +0300

    k

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'Classes/Projects/tab_bar/tab_bar_basic_interview_1/interview.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InterviewPage(),
+      home: null,
     );
   }
 }
