2ca02adbee377820f29810a58288ff12089633aa
MertSaygili <mert71719601@gmail.com>
2022-07-29 14:46:03 +0300

    custom font

--- lib/Uplabs/login_panels/login_0/login_page_view.dart
+++ lib/Uplabs/login_panels/login_0/login_page_view.dart
@@ -9,16 +9,59 @@ class LoginPageViewOne extends StatefulWidget {
 
 class _LoginPageViewOneState extends State<LoginPageViewOne> {
   final String _loginTitle = 'Login';
+  late final double _screenWidth = MediaQuery.of(context).size.width;
+  late final double _screenHeight = MediaQuery.of(context).size.height;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 80.0, horizontal: 45),
+        padding: const EdgeInsets.symmetric(vertical: 60.0, horizontal: 20),
         child: Column(
           children: [
-            Text(
-              _loginTitle,
+            Stack(
+              children: [
+                Container(
+                  height: _screenHeight / 6,
+                  width: _screenWidth,
+                  color: Colors.transparent,
+                ),
+                Positioned(
+                  top: 5,
+                  left: _screenWidth / 3,
+                  child: Container(
+                    height: 80,
+                    width: _screenWidth / 4,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.orangeAccent,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 5,
+                  left: _screenWidth / 8,
+                  child: Container(
+                    height: 100,
+                    width: _screenWidth / 2,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: _screenHeight / 22,
+                  left: _screenWidth / 2.9,
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                        // fontFamily: 'RobotoMono',
+                        //fontWeight: FontWeight.w500,
+                        fontSize: 24),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
@@ -33,8 +76,9 @@ class CustomTheme {
   CustomTheme() {
     theme = ThemeData(
       textTheme: const TextTheme(
-        headline3: TextStyle(
-          color: Colors.pink,
+        headline5: TextStyle(
+          color: Colors.white,
+          fontSize: 30,
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: CustomTheme().theme,
       home: const LoginPageViewOne(),
     );
   }
