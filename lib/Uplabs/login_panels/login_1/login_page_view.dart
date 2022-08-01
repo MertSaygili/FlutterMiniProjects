3e170e04cc357ff3bb88ced6822dc114af4fddc0
MertSaygili <mert71719601@gmail.com>
2022-08-01 18:53:16 +0300

    text is done working on teXT FIELD

--- lib/Uplabs/login_panels/login_1/login_page_view.dart
+++ lib/Uplabs/login_panels/login_1/login_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project1_change_appbar_color/Uplabs/login_panels/login_0/login_page_view.dart';
 
 import 'Data/data.dart';
 
@@ -10,13 +11,48 @@ class LoginPageView extends StatefulWidget {
 }
 
 class _LoginPageViewState extends State<LoginPageView> {
+  final String _userNameHintText = 'Username';
+  final String _userPasswordHintText = 'Password';
+
   @override
   Widget build(BuildContext context) {
     final Text _title = Text(
       Strings().loginText,
       style: Theme.of(context).textTheme.headline3,
     );
-
+    final RichText _text = RichText(
+      maxLines: 2,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: Strings().subtitleTextPart_1,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          WidgetSpan(
+            child: GestureDetector(
+              onTap: () {},
+              child: Text(Strings().subtitleTextPart_2),
+            ),
+          ),
+          TextSpan(
+            text: Strings().subtitleTextPart_3,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ],
+      ),
+    );
+    final Padding _userName = Padding(
+      padding: PaddingItems().paddingVerticalNormal,
+      child: TextFormField(
+        keyboardType: TextInputType.name,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          suffixIcon: UsedIcons().iconPerson,
+          hintText: _userNameHintText,
+          hintStyle: Theme.of(context).textTheme.button,
+        ),
+      ),
+    );
     return Scaffold(
       body: Center(
         child: ListView(
@@ -24,6 +60,8 @@ class _LoginPageViewState extends State<LoginPageView> {
           padding: PaddingItems().paddingPage,
           children: [
             _title,
+            _text,
+            _userName,
           ],
         ),
       ),
@@ -31,14 +69,24 @@ class _LoginPageViewState extends State<LoginPageView> {
   }
 }
 
+class UsedIcons {
+  Icon iconPerson = const Icon(Icons.person);
+  Icon iconPassword = const Icon(Icons.lock);
+}
+
 class PaddingItems {
   final EdgeInsets paddingPage = EdgeInsets.symmetric(
     horizontal: PaddingValues().paddingPageHorizontal,
     vertical: PaddingValues().paddingPageVertical,
+  );
+
+  final EdgeInsets paddingVerticalNormal = EdgeInsets.only(
+    top: PaddingValues().paddingNormalVertical,
   );
 }
 
 class PaddingValues {
   final double paddingPageVertical = 30;
   final double paddingPageHorizontal = 30;
+  final double paddingNormalVertical = 20;
 }
--- lib/Uplabs/login_panels/login_1/theme.dart
+++ lib/Uplabs/login_panels/login_1/theme.dart
@@ -7,21 +7,29 @@ class CustomTheme {
     theme = ThemeData(
       fontFamily: 'RobotoMono',
       textTheme: TextTheme(
-          headline3: TextStyle(
-            fontSize: FontSizes()._titleFontSize,
-            fontWeight: FontWeight.bold,
-            color: AllColors().colorBlack,
-          ),
-          bodyText1: TextStyle(
-            fontSize: FontSizes()._textFontSize,
-            color: AllColors().colorDarkGrey,
-            wordSpacing: -5,
-          ),
-          bodyText2: TextStyle(
-            fontSize: FontSizes()._textFontSize,
-            color: AllColors().colorBlue,
-            wordSpacing: -5,
-          )),
+        headline3: TextStyle(
+          fontSize: FontSizes()._titleFontSize,
+          fontWeight: FontWeight.bold,
+          color: AllColors().colorBlack,
+        ),
+        bodyText1: TextStyle(
+          fontSize: FontSizes()._textFontSize,
+          fontWeight: FontWeight.w600,
+          color: AllColors().colorDarkGrey,
+          wordSpacing: -5,
+        ),
+        bodyText2: TextStyle(
+          fontSize: FontSizes()._textFontSize,
+          fontWeight: FontWeight.w700,
+          color: AllColors().colorBlue,
+          wordSpacing: -5,
+        ),
+        button: TextStyle(
+          fontSize: FontSizes()._textFontSize,
+          color: AllColors().colorDarkGrey,
+          letterSpacing: 1,
+        ),
+      ),
     );
   }
 }
