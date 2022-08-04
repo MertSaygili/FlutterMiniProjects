eaf9f0593372b848b6bf560466a4da2373f33663
MertSaygili <mert71719601@gmail.com>
2022-08-04 18:49:15 +0300

    deleting line

--- lib/Classes/Projects/text_file/read_write.dart
+++ lib/Classes/Projects/text_file/read_write.dart
@@ -5,7 +5,6 @@ import "package:path_provider/path_provider.dart";
 class CounterStorage {
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
-
     return directory.path;
   }
 
