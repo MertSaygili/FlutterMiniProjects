eefb75d3822f66dcceb9cf9a6d5ffeeeb2cad55a
MertSaygili <mert71719601@gmail.com>
2022-08-07 19:20:10 +0300

    almost done with shared_preferences_manager class

--- lib/PubDev/shared_preferences/shared_preferences_manager.dart
+++ lib/PubDev/shared_preferences/shared_preferences_manager.dart
@@ -1,7 +1,33 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+enum SharedItemNames {
+  name,
+  phone,
+  personCount,
+  reservationDate,
+  comment,
+}
+
 class SharedPreferencesManager {
   SharedPreferences? preferences;
 
-  void setInt() {}
+  Future<void> initialize() async {
+    preferences = await SharedPreferences.getInstance();
+  }
+
+  void isInitialize() {
+    if (preferences == null) {
+      throw 'Preferences has not initialized';
+    }
+  }
+
+  void setInt(SharedItemNames key, int value) {
+    preferences?.setInt(key.name, value);
+  }
+
+  void setString(SharedItemNames key, String value) {
+    preferences?.setString(key.name, value);
+  }
+
+  void setDateTime(SharedItemNames key, String value) {}
 }
