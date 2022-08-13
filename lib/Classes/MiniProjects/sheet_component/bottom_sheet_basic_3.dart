d2ca5e3e62e72b9091be7db2e743a36d14922e2c
MertSaygili <mert71719601@gmail.com>
2022-08-13 19:04:26 +0300

    listview with sheet component

---
+++ lib/Classes/MiniProjects/sheet_component/bottom_sheet_basic_3.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetBasicThird extends StatefulWidget {
+  const BottomSheetBasicThird({Key? key}) : super(key: key);
+
+  @override
+  State<BottomSheetBasicThird> createState() => _BottomSheetBasicThirdState();
+}
+
+enum ListTileTitles { Photo, Music, Video, Share }
+
+class _BottomSheetBasicThirdState extends State<BottomSheetBasicThird> {
+  final String _title = 'Modal Bottom Sheet';
+  final String _buttonText = 'Click me';
+  final String _bodyText = 'Modal Bottom Sheet Example';
+  final List<Icon> _icons = const [
+    Icon(Icons.photo),
+    Icon(Icons.music_note_sharp),
+    Icon(Icons.video_camera_front),
+    Icon(Icons.share),
+  ];
+
+  final List<String> _titles = ['Photo', 'Music', 'Video', 'Share'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_bodyText),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+          ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: ListTileTitles.values.length,
+                        itemBuilder: (BuildContext context, index) {
+                          return _customCard(index, context);
+                        },
+                      );
+                    });
+              },
+              child: Text(_buttonText)),
+        ],
+      )),
+    );
+  }
+
+  Card _customCard(int index, BuildContext context) {
+    return Card(
+      elevation: 10,
+      color: const Color.fromARGB(255, 238, 237, 237),
+      child: ListTile(
+        leading: _icons.elementAt(index),
+        title: Text(
+          _titles.elementAt(index),
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        onTap: () {},
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Classes/MiniProjects/dialog/alert_learn.dart';
+import 'Classes/MiniProjects/sheet_component/bottom_sheet_basic_3.dart';
 import 'Uplabs/Calculator/calculator_shared_preferences_0/themes/custom_theme.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: CustomTheme().theme,
-      home: const AlertLearnView(),
+      home: const BottomSheetBasicThird(),
     );
   }
 }
