2547af5c8c3af44ff63da4755029dbeda4490b79
MertSaygili <mert71719601@gmail.com>
2022-10-03 19:31:23 +0300

    example of sliver appbar

--- lib/Classes/MiniProjects/silver_appbar/silver_appbar_1/silver_appbar_main.dart
+++ lib/Classes/MiniProjects/silver_appbar/silver_appbar_1/silver_appbar_main.dart
@@ -10,9 +10,29 @@ class SilverAppbarView extends StatefulWidget {
 class _SilverAppbarViewState extends State<SilverAppbarView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: CustomScrollView(
-        slivers: [],
+        slivers: [
+          SliverPersistentHeader(
+            delegate: CustomSilverAppbarDelegate(expandedHeight: 200),
+            pinned: true,
+            floating: true,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100.0,
+                  child: Center(
+                    child: Text('$index', textScaleFactor: 5),
+                  ),
+                );
+              },
+              childCount: 20,
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -22,14 +42,50 @@ class CustomSilverAppbarDelegate extends SliverPersistentHeaderDelegate {
   CustomSilverAppbarDelegate({required this.expandedHeight});
 
   final double expandedHeight;
+  final Icon _iconSettings = const Icon(Icons.settings);
 
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Stack(
-      children: const [],
+      children: [
+        _buildAppbar(shrinkOffset, context),
+        _buildSliverAppbar(shrinkOffset, context),
+      ],
     );
   }
+
+  Widget _buildAppbar(double shrinkOffset, BuildContext context) {
+    return Opacity(
+      opacity: appear(shrinkOffset),
+      child: AppBar(
+        title: const Text('Title'),
+        actions: [_iconSettings],
+      ),
+    );
+  }
+
+  Widget _buildSliverAppbar(double shrinkOffset, BuildContext context) {
+    return Opacity(
+      opacity: disappear(shrinkOffset),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Text('Title', style: Theme.of(context).textTheme.headline3),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: _iconSettings,
+          )
+        ],
+      ),
+    );
+  }
+
+  double appear(double shrinkOffset) => shrinkOffset / expandedHeight;
+
+  double disappear(double shrinkOffset) => 1 - shrinkOffset / expandedHeight;
 
   @override
   double get maxExtent => expandedHeight;
