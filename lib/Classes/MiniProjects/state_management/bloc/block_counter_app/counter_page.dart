6beba0c19aeb7e58e414987d4a931ca2c76b3264
MertSaygili <mert71719601@gmail.com>
2022-12-21 11:40:20 +0300

    added state folder

---
+++ lib/Classes/MiniProjects/state_management/bloc/block_counter_app/counter_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({Key? key}) : super(key: key);
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
