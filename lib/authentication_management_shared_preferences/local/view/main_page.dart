8f85a8584c32ba0c23f5f4055d42a7a758ac37bc
MertSaygili <mert71719601@gmail.com>
2023-01-08 01:50:51 +0300

    sikecem

---
+++ lib/authentication_management_shared_preferences/local/view/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainPageView extends StatefulWidget {
+  const MainPageView({Key? key}) : super(key: key);
+
+  @override
+  State<MainPageView> createState() => _MainPageViewState();
+}
+
+class _MainPageViewState extends State<MainPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:project1_change_appbar_color/Classes/MiniProjects/state_management/bloc/block_counter_app/counter_page.dart';
+import 'package:project1_change_appbar_color/authentication_management_shared_preferences/local/shared/shared_preferences_manager.dart';
+import 'package:project1_change_appbar_color/authentication_management_shared_preferences/local/view/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 // trying something
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final SharedManager sharedManager = SharedManager();
+  late Widget x;
 
   @override
-  Widget build(BuildContext context) {
+  MaterialApp build(BuildContext context) {
+    _checkValidation();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.light(),
-      home: const CounterPage(),
+      home: LoginPageSharedManagement(),
     );
+  }
+
+  void _checkValidation() async {
+    bool result = await sharedManager.getBool(SharedKeys.hasUserEnter);
+    print(result);
   }
 }
