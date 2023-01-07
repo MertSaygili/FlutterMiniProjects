03b2838601d63600b67324bff26bd448eacf0f62
MertSaygili <mert71719601@gmail.com>
2023-01-08 00:20:59 +0300

    state_management_not_finished

--- lib/Classes/MiniProjects/state_management/bloc/block_counter_app/counter_page.dart
+++ lib/Classes/MiniProjects/state_management/bloc/block_counter_app/counter_page.dart
@@ -8,25 +8,29 @@ class CounterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Counter')),
-      body: BlocBuilder<CounterCubit, int>(
-        builder: (context, count) => Center(child: Text('$count')),
-      ),
-      floatingActionButton: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () => context.read<CounterCubit>().increment(),
-          ),
-          const SizedBox(height: 4),
-          FloatingActionButton(
-            child: const Icon(Icons.remove),
-            onPressed: () => context.read<CounterCubit>().decrement(),
-          ),
-        ],
+    return BlocProvider(
+      lazy: false,
+      create: (_) => CounterCubit(),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Counter')),
+        body: BlocBuilder<CounterCubit, int>(
+          builder: (context, count) => Center(child: Text('$count')),
+        ),
+        floatingActionButton: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () => context.read<CounterCubit>().increment(),
+            ),
+            const SizedBox(height: 4),
+            FloatingActionButton(
+              child: const Icon(Icons.remove),
+              onPressed: () => context.read<CounterCubit>().decrement(),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
-// import 'package:flutter/material.dart';
-// import 'Classes/MiniProjects/skeleteons/basic_skeleton.dart';
+import 'package:flutter/material.dart';
+import 'package:project1_change_appbar_color/Classes/MiniProjects/state_management/bloc/block_counter_app/counter_page.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-//   runApp(const MyApp());
-// }
+  runApp(const MyApp());
+}
 
-// // trying something
+// trying something
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData.light(),
-//       home: const BasicSkeleton(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.light(),
+      home: const CounterPage(),
+    );
+  }
+}
