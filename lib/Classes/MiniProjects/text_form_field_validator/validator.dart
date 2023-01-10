78d23762c7213f868d6f6c53b6ea858be790fdb8
MertSaygili <mert71719601@gmail.com>
2023-01-10 23:10:37 +0300

    validator bug solved

--- lib/Classes/MiniProjects/text_form_field_validator/validator.dart
+++ lib/Classes/MiniProjects/text_form_field_validator/validator.dart
@@ -4,7 +4,7 @@ class Validator {
   String? validateEmail(String? value) {
     if (value == null || value.isEmpty) {
       return 'Please enter some text';
-    } else if (Regex().checkEmail(value)) {
+    } else if (!Regex().checkEmail(value)) {
       return 'Please enter a valid email address';
     } else {
       return null;
