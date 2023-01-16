9f495b816fd9159370fecc2282901aedd142c2fc
MertSaygili <mert71719601@gmail.com>
2023-01-16 13:11:03 +0300

    notification file added

---
+++ lib/Classes/MiniProjects/noitification/basic_notification_0/notification_basic.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationBasicApp extends StatefulWidget {
+  const NotificationBasicApp({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationBasicApp> createState() => _NotificationBasicAppState();
+}
+
+class _NotificationBasicAppState extends State<NotificationBasicApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
